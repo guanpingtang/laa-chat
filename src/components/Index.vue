b9237ad1336fdb6e98027e8b39a7dab126b4ac00
lvzhen <982463748@qq.com>
2018-05-24 14:22:21 +0800

    demo

---
+++ src/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
